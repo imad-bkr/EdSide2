e3d206815e800c22267f79c4524abb9ea37c9f22
imad-bkr <kariimad@yahoo.fr>
2021-05-23 20:25:30 +0200

    event modified

--- models/Calendar/Event.php
+++ models/Calendar/Event.php
@@ -4,16 +4,16 @@ namespace Calendar;
 
 class Event {
 
-    private $id;
-    private $name;
+    private $id_evenement;
+    private $nom;
     private $description;
-    private $start;
-    private $end;
+    private $date_debut;
+    private $date_fin;
 
-    public function getId(): int { return $this->id; }
-    public function getName (): ?string { return $this->name; }
+    public function getId(): int { return $this->id_evenement; }
+    public function getName (): ?string { return $this->nom; }
     public function getDescription (): string { return $this->description ?? ''; }
-    public function getStart (): \DateTime { return new \DateTime($this->start); }
-    public function getEnd (): \DateTime { return new \DateTime($this->end); }
+    public function getStart (): \DateTime { return new \DateTime($this->date_debut); }
+    public function getEnd (): \DateTime { return new \DateTime($this->date_fin); }
 
 }
