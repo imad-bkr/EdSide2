61dfd02b9b1b381d7240e06f59828e524916d710
imad-bkr <kariimad@yahoo.fr>
2021-05-23 22:41:26 +0200

    commit

--- controllers/backend.controller.php
+++ controllers/backend.controller.php
@@ -156,27 +156,7 @@ function getPageCalendarNewEvent() {
         header("Location:".URL."accueil");
     }
 
-    function getGroupesFromDB(){
-        $bdd = connexionPDO();
-        $req = "SELECT * FROM groupe";
-        $stmt = $bdd->prepare($req);
-        $stmt->execute();
-        $groupes = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-        $stmt->closeCursor();
-        return $groupes;
-    }
-
-    function getGroupeFromDB($group){
-        $bdd = connexionPDO();
-        $req = "SELECT * FROM groupe
-        WHERE nom = :nom";
-        $stmt = $bdd->prepare($req);
-        $stmt->bindValue(":nom", $group, PDO::PARAM_STR);
-        $stmt->execute();
-        $groupe = $stmt->fetch(\PDO::FETCH_ASSOC);
-        $stmt->closeCursor();
-        return $groupe;
-    }
+    require_once "models/groups.dao.php";
 
     if(Securite::verificationAccess()) {
         require 'models/Calendar/Events.php';
