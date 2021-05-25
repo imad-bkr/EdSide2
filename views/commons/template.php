d04fd59bb3d7564618f7c12e3838bee2d5c944b6
TaYFoN <TaYFoN@users.noreply.github.com>
2021-05-25 11:37:42 +0200

    footer

--- views/commons/template.php
+++ views/commons/template.php
@@ -10,13 +10,18 @@
     <link rel="stylesheet" href=<?= URL . $css ?>>
     <link rel="icon" href="<?= URL ?>public/img/favicon.ico" sizes="32x32">
 </head>
+
 <body>
     <header class="header">
         <a href="<?= URL ?>tutoring/browse"><img class="logo-mini" src="<?= URL ?>public/img/logo.png" alt="EdSide"></a>
-        <div class="user"><?php echo $_SESSION['user'];?></div>
+        <div class="user"><?php echo $_SESSION['user']; ?></div>
         <ul class="user-menu hidden">
-            <a href="<?= URL ?>settings"><li>Paramètres</li></a>
-            <a href=""><li>English</li></a>
+            <a href="<?= URL ?>settings">
+                <li>Paramètres</li>
+            </a>
+            <a href="">
+                <li>English</li>
+            </a>
             <form method="POST" action="">
                 <input type="hidden" name="deconnexion" value="true" />
                 <input class="log-out" type="submit" value="Déconnexion" />
@@ -25,8 +30,10 @@
     </header>
     <?php include("navbar.php"); ?>
     <?= $content ?>
-    <footer class="footer">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur eligendi tempora corporis rem maiores architecto adipisci veritatis sunt aperiam, nemo exercitationem dignissimos dolor, iste assumenda explicabo dicta expedita autem temporibus.</footer>
+    <footer class="footer">
+        <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
+    </footer>
     <script src="<?= URL ?>public/js/layout.js"></script>
 </body>
-</html>
 
+</html>
