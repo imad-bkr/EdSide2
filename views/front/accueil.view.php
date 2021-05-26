601e31f0ed8cd0b4c9a427a266f129c7f0298976
imad-bkr <kariimad@yahoo.fr>
2021-05-26 10:31:25 +0200

    c

--- views/front/accueil.view.php
+++ views/front/accueil.view.php
@@ -21,14 +21,14 @@
             <img class="logo" src="public/img/logo.png" alt="Edside">
             <div class="edside-presentation">
                 <h1>Edside, get better organized</h1>
-                <p>Edside est une plateforme de gestion de cours créée par des étudiants de l'EFREI pour des étudiants de l'EFREI.<br>Vous y trouverez des outils permettant une meilleure organisation de votre scolarité.</p>
+                <p>Edside est une plateforme de gestion de cours créée par des étudiants de l'EFREI pour des étudiants de l'EFREI. Vous y trouverez des outils permettant une meilleure organisation de votre scolarité.</p>
             </div>
         </section>
         <section class="info-calendar">
             <img class="section-img" src="public/tmp/calendrier.PNG" alt="calendar">
             <div class="section-text">
                 <h2 class="section-title">CALENDRIER</h2>
-                <p>calendrier synchronisé qui permetaux utilisateursindividuellement, mais aussi en groupe,de visualiser chronologiquement des évènementsprévus au préalable. La synchronisation sert à tenir tous les membres d’un groupe informés en temps et en heure de toutes les informations relatives à un évènement</p>
+                <p>Edside propose un calendrier synchronisé qui permet aux utilisateurs individuellement, mais aussi en groupe,de visualiser chronologiquement des évènements prévus au préalable. La synchronisation sert à tenir tous les membres d’un groupe informés en temps et en heure de toutes les informations relatives à un évènement</p>
             </div>
         </section>
         <section class="info-simulator">
